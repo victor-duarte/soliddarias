12a2b3b21b5c2dabdce130cccbf5f150901b23dc
victor-duarte <vicsalf@gmail.com>
2023-03-15 11:26:33 -0500

    Add Tailwindcss sort class plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,13 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={styles.main}>
+      <form className="red-800 flex flex-col">
+        <label htmlFor="loginEmail">Correo</label>
+        <input id="loginEmail" type="text" />
+        <label htmlFor="loginPassword">Contraseña</label>
+        <input id="loginPassword" type="text" />
+        <button>Entrar</button>
+      </form>
       <a
         href="https://soliddarias.org/"
         rel="noreferrer noopener"
