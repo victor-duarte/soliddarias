40f95ac8608ea999a1a431380addb05497099652
victor-duarte <vicsalf@gmail.com>
2023-03-17 13:56:17 -0500

    Add dashboard page

---
+++ src/app/components/Dashboard.tsx
@@ -0,0 +1,36 @@
+import styles from "@/app/page.module.css";
+import {
+  CalendarDaysIcon,
+  PhotoIcon,
+  UserCircleIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+const Dashboard = () => {
+  return (
+    <main className={styles.main}>
+      <h1 className="text-3xl font-bold underline">Dashboard</h1>
+      <div className="mt-10 flex max-w-sm flex-wrap gap-y-8">
+        <Link href="#" className="flex w-1/2 flex-col items-center">
+          <UserCircleIcon className="inline-block h-20 w-20" />
+          Perfil
+        </Link>
+        <Link href="#" className="flex w-1/2 flex-col items-center">
+          <CalendarDaysIcon className="inline-block h-20 w-20" />
+          Eventos
+        </Link>
+        <Link href="#" className="flex w-1/2 flex-col items-center">
+          <UserGroupIcon className="inline-block h-20 w-20" />
+          Grupos
+        </Link>
+        <Link href="#" className="flex w-1/2 flex-col items-center">
+          <PhotoIcon className="inline-block h-20 w-20" />
+          Imágenes
+        </Link>
+      </div>
+    </main>
+  );
+};
+
+export default Dashboard;
